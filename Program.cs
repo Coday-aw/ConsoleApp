dc9dadb690537d950b9ddee4aa65f83e9431e89b
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2025-09-10 10:50:46 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("userName:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+
+            Console.WriteLine($"welcome back, {userName}!");
+
+        }
+    }
+}
